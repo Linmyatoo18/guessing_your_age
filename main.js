a59c7f8ff7483ac34c1258ad3273f0b385955697
Linmyatoo <108082128+Linmyatoo18@users.noreply.github.com>
2022-12-15 21:56:15 +0630

    main

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ let age;
 let txt = "You are ";
 let years = " years old";
 let year = " year old";
+let emoji = "💀";
 
 document.getElementById("submit").onclick = function(){
     age = document.getElementById("input").value;
     console.log(age);
     
     if(age > 1){
-        window.alert(txt + age + years);
+        window.alert(txt + age + years + emoji);
     }
     else if(age == 1){
-        window.alert(txt + age + year);
+        window.alert(txt + age + year + emoji);
     }
     else{
-        window.alert("Invalid input!");
+        window.alert("Invalid input!" + emoji);
     }
 
 }
